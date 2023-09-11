84feae33dc68e3629f26320d48514de35244feb1
Tarun Khare <tarunkh1690@gmail.com>
2023-09-11 11:46:58 +0530

    added outputs

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = try(aws_vpc.this[0].id, null)
+  value       = try(module.Demo_vpc.aws_vpc.this[0].id, null)
 }
 
 output "public_subnets" {
   description = "List of IDs of public subnets"
-  value       = aws_subnet.public[*].id
+  value       = module.Demo_vpc.aws_subnet.public[*].id
 }
 
 output "private_subnets" {
   description = "List of IDs of private subnets"
-  value       = aws_subnet.private[*].id
+  value       = module.Demo_vpc.aws_subnet.private[*].id
 }
